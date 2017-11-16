d999dc89e224b037ed40eae5f5aaaf64cb20c33d
dpopkov <pkvdenis@gmail.com>
2017-11-16 20:05:45 +0300

    Add Spring Security in DB.

--- src/main/java/io/khasang/training_hotel/controller/AppController.java
+++ src/main/java/io/khasang/training_hotel/controller/AppController.java
@@ -2,9 +2,12 @@ package io.khasang.training_hotel.controller;
 
 import io.khasang.training_hotel.model.CreateTable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -39,5 +42,11 @@ public class AppController {
     @RequestMapping("/admin")
     public String adminPage() {
         return "admin-response-body";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = {"/password/{password}"}, method = RequestMethod.GET)
+    public String getPassword(@PathVariable("password") String password) {
+        return new BCryptPasswordEncoder().encode(password);
     }
 }
