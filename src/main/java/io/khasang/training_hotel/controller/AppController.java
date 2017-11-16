6ab9c12e8d53a4fe3421b585aaad4d0380190a5c
dpopkov <pkvdenis@gmail.com>
2017-11-16 15:09:50 +0300

    Add Spring Security in memory.

--- src/main/java/io/khasang/training_hotel/controller/AppController.java
+++ src/main/java/io/khasang/training_hotel/controller/AppController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AppController {
@@ -26,5 +27,17 @@ public class AppController {
     public String tableCreationInfo(Model model) {
         model.addAttribute("status", createTable.createTableStatus());
         return "status";
+    }
+
+    @ResponseBody
+    @RequestMapping("/user")
+    public String userPage() {
+        return "user-response-body";
+    }
+
+    @ResponseBody
+    @RequestMapping("/admin")
+    public String adminPage() {
+        return "admin-response-body";
     }
 }
